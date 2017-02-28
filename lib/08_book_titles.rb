493b2f1ca16c0949a3084721b15b5b65f24504ec
Slip <smurf6258@yahoo.com>
2017-02-28 16:57:07 -0500

    passed all rspec tests

--- lib/08_book_titles.rb
+++ lib/08_book_titles.rb
@@ -1,3 +1,30 @@
 class Book
-  # TODO: your code goes here!
+  attr_reader :title
+
+
+  def title=(t)
+     @title = titleize(t)
+  end
+
+ def titleize(str)
+  capitalized_array = []
+
+  str.split.each_with_index do |word, index|
+    capitalized_array << capitalize_word(word, (index == 0))
+  end
+
+  capitalized_array.join(" ")
+end
+
+LITTLE_WORDS = ["and", "or", "the", "over", "of", "in", "a", "an"]
+
+def capitalize_word(str, force_cap)
+
+  return str if LITTLE_WORDS.include?(str) && !force_cap
+
+  char_array = str.split("")
+  char_array[0] = char_array[0].upcase
+  char_array.join("")
+end
+
 end
